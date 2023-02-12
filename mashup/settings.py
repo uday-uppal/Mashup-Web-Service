09ca89c40fe2db4696cacbb70054bbe94ff91818
Uday Uppal <81161758+uday-uppal@users.noreply.github.com>
2023-02-12 23:32:49 +0530

    Update settings.py

--- mashup/settings.py
+++ mashup/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tiazlce=o&q4e0a3g84g02mx*$55o7euzb8tjwa#8iy#_pebd$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 # STATICFILES_DIRS=(os.path.join(BASE_DIR,"public/static"))
 # Default primary key field type
